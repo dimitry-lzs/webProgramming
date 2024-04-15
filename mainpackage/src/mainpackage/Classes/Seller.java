3a13b4e7a7524365753f90457ef4b181b0d3e5b8
YelAq <napat.rojo7@gmail.com>
2024-04-15 20:47:08 +0300

    Update Seller.java

--- mainpackage/src/mainpackage/Classes/Seller.java
+++ mainpackage/src/mainpackage/Classes/Seller.java
@@ -2,17 +2,27 @@ package mainpackage.Classes;
 import mainpackage.enums.UserType;
 
 public class Seller extends User {
+
+    Client clientarray[];
+    PhoneNumber numberarray[];
+
     public Seller() {
         super(UserType.SELLER);
     }
 
-    public void addClient(){
-    
+    public void addClient(PhoneNumber phonenum){
+        System.out.println("Adding client...");
+
+        //Creating new phone number.
+        Client newclient = new Client();
+        
+
+
     }
     public void issueUser(){
-    
+        System.out.println("Issuing user...");
     }
     public void changeClientProgram(){
-    
+        System.out.println("Changing client program...");
     }
 }
