61c42afa68d07e6e84b94ef799111ea3766b709f
dimitry-lzs <lazanasdimitrios@gmail.com>
2024-04-15 20:34:37 +0300

    Add Bill-related functionality to Client class and refactor Program class

--- mainpackage/src/mainpackage/Classes/Client.java
+++ mainpackage/src/mainpackage/Classes/Client.java
@@ -4,10 +4,37 @@ import mainpackage.enums.UserType;
 public class Client extends User {
     private String afm;
     private PhoneNumber phonenumber;
+    private Bill[] bills;   // An array that contains bills.
 
     public Client(String afm, PhoneNumber phonenumber) {
         super(UserType.CLIENT);
         this.afm = afm;
         this.phonenumber = phonenumber;
+    }
+
+    public void setBills(Bill[] bills) {
+        this.bills = bills;
+    }
+
+    public String getAfm() {
+        return afm;
+    }
+
+    public PhoneNumber getPhonenumber() {
+        return phonenumber;
+    }
+
+    public Bill[] getBills() {
+        return bills;
+    }
+
+    public void viewBills() {
+        for (int i = 0; i < bills.length; i++) {
+            bills[i].printBill();
+        }
+    }
+
+    public void viewLastBill() {
+        bills[bills.length - 1].printBill();
     }
 }
--- mainpackage/src/mainpackage/Classes/Program.java
+++ mainpackage/src/mainpackage/Classes/Program.java
@@ -5,7 +5,7 @@ public class Program {
 
     private String ProgramName;
     private int CallTime;
-    private int Pagio;  //Idk
+    private int fee;  //Ik
 
     public String getProgramName() {
         return ProgramName;
@@ -22,24 +22,24 @@ public class Program {
     }
 
     public int getPagio() {
-        return Pagio;
+        return fee;
     }
-    public void setPagio(int pagio) {
-        this.Pagio = pagio;
+    public void setPagio(int fee) {
+        this.fee = fee;
     }
 
-    public Program(String programName, int callTime, int pagio) {
-        ProgramName = programName;
-        CallTime = callTime;
-        Pagio = pagio;
+    public Program(String programName, int callTime, int fee) {
+        this.ProgramName = programName;
+        this.CallTime = callTime;
+        this.fee = fee;
     }
 
     public void printProgram() {
         System.out.println(
             "PROGRAM INFO\n" +
             "Name: " + ProgramName + "\n" +
-            "Call Time: "+ CallTime + "\n" +
-            "Invoice: "+ Pagio + "\n" +
+            "Call Time: " + CallTime + "\n" +
+            "Invoice: " + fee + "\n" +
             "\n"
             );
     }
