82f82d96b5ea5abfcc9817e0ee5dda12cc0d5c21
dimitry-lzs <lazanasdimitrios@gmail.com>
2024-06-03 18:53:42 +0300

    fix drop

--- src/main/java/com/webProgramming/Classes/Util.java
+++ src/main/java/com/webProgramming/Classes/Util.java
@@ -30,7 +30,7 @@ public class Util {
 
                 settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
 
-                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+                settings.put(Environment.HBM2DDL_AUTO, "update");
 
                 configuration.addAnnotatedClass(User2.class);
                 configuration.setProperties(settings);
