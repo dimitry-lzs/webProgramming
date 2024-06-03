cb8ab2fc6852d73900dbfbfe42559bc2c66d8490
ANTONIOS TSALMPOURIS <antonis.tsal@gmail.com>
2024-06-03 21:48:41 +0300

    added comments to stop problems

--- src/main/java/com/webProgramming/Classes/Seller.java
+++ src/main/java/com/webProgramming/Classes/Seller.java
@@ -1,11 +1,11 @@
 package com.webProgramming.Classes;
-import java.util.Set;
+//import java.util.Set;
 
 import com.webProgramming.enums.UserType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+//import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
