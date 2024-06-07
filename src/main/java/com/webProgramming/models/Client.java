610f389ac51096ea4033a7adde270b04ba0dd019
dimitry-lzs <lazanasdimitrios@gmail.com>
2024-06-07 22:29:49 +0300

    remove useless import

--- src/main/java/com/webProgramming/models/Client.java
+++ src/main/java/com/webProgramming/models/Client.java
@@ -2,8 +2,6 @@ package com.webProgramming.models;
 
 import java.util.Set;
 
-import com.webProgramming.models.enums.UserType;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
